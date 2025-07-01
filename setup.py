0ae04698b7ddbd6f7c245cd0374af99f015c6f7c
Ander Alvarez Sanz <aalvarezsanz@ideko.es>
2025-07-01 13:57:00 +0200

    Cambio setup.py.

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='idkROM',
+    name='idkrom',
     version='0.1.0',  # You can adjust the version number as needed
     description='A Reduced Order Modeling (ROM) framework',
     author='Ander Alvarez Sanz',  # Replace with your name
