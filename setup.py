acb83b1cf537da3977a4894018f82fd994d6fa07
Ander Alvarez Sanz <aalvarezsanz@ideko.es>
2025-07-02 07:36:06 +0200

    .

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     version='0.1.0',  # You can adjust the version number as needed
     description='A Reduced Order Modeling (ROM) framework',
     author='Ander Alvarez Sanz',  # Replace with your name
-    author_email='aalvarezsanz@ideko.es',  # Replace with your email
+    author_email='andersz.alvarez@gmail.com',  # Replace with your email
     packages=find_packages(where="src"),
     package_dir={"": "src"},
 )
